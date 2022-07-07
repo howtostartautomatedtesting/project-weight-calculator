627b30e8d37f4558cf1e3bbdb9c8e6650b8c2716
Kirill Vasilenko <wetanos@gmail.com>
2022-07-07 20:24:33 +0300

    test: fill inputs

--- src/test/java/WeightCalculatorTest.java
+++ src/test/java/WeightCalculatorTest.java
@@ -1,4 +1,4 @@
-import org.junit.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,10 +9,21 @@ public class WeightCalculatorTest {
 
     @Test
     public void openPageTest(){
+        By inputNameLocator = By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input");
+        By inputHeightLocator = By.xpath("//input[@name='height']");
+        By inputWeightLocator = By.name("weight");
+        String name = "IVAN";
+        String height = "183";
+        String weight = "80";
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://svyatoslav.biz/testlab/wt/index.php");
-        WebElement inputName =  driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input"));
-        inputName.sendKeys("IVAN");
+        WebElement inputName =  driver.findElement(inputNameLocator);
+        inputName.sendKeys(name);
+        WebElement inputHeight =  driver.findElement(inputHeightLocator);
+        inputHeight.sendKeys(height);
+        WebElement inputWeight =  driver.findElement(inputWeightLocator);
+        inputWeight.sendKeys(weight);
         driver.quit();
     }
 }
