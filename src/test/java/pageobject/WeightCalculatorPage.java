be54889b7cd90de5d420d1e0d368d95c9a80d364
Kate Miholenko <katemixa92@gmail.com>
2022-07-07 21:47:59 +0300

    refactor: add class WeightCalculatorPage

---
+++ src/test/java/pageobject/WeightCalculatorPage.java
@@ -0,0 +1,43 @@
+package pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WeightCalculatorPage {
+    WebDriver driver;
+    By inputNameLocator = By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input");
+    By inputHeightLocator = By.xpath("//input[@name='height']");
+    By inputWeightLocator = By.name("weight");
+    By inputGenderLocator = By.xpath("//input[@value='m']");
+    By buttonSubmitLocator = By.xpath("//input[@type='submit']");
+
+    public WeightCalculatorPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void fillInputName(String name) {
+        WebElement inputName = driver.findElement(inputNameLocator);
+        inputName.sendKeys(name);
+    }
+
+    public void fillInputHeight(String height) {
+        WebElement inputHeight = driver.findElement(inputHeightLocator);
+        inputHeight.sendKeys(height);
+    }
+
+    public void fillInputWeight(String weight) {
+        WebElement inputWeight = driver.findElement(inputWeightLocator);
+        inputWeight.sendKeys(weight);
+    }
+
+    public void selectMaleGender() {
+        WebElement inputGender = driver.findElement(inputGenderLocator);
+        inputGender.click();
+    }
+
+    public void clickButtonSubmit() {
+        WebElement buttonSubmit = driver.findElement(buttonSubmitLocator);
+        buttonSubmit.click();
+    }
+}
